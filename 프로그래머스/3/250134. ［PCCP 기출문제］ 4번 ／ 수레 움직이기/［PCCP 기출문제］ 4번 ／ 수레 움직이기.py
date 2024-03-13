ed2742b3ee85b965f884fe0f48c71fb9bcfb329c
gamer8787 <62703390+gamer8787@users.noreply.github.com>
2024-03-13 14:54:36 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수레 움직이기, Time: 0.27 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.py
+++ 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.py
@@ -1,67 +1,58 @@
-DIR = [(0, -1), (0, 1), (1, 0), (-1, 0)]
+from collections import deque
+
+dy = [-1, 0, 1, 0]
+dx = [0, 1, 0, -1]
+
 
 def solution(maze):
-    n, m = len(maze), len(maze[0])
-    red, blue, target_red, target_blue = find_positions(maze)
+    r = len(maze)
+    c = len(maze[0])
+    for i in range(r):
+        for j in range(c):
+            if maze[i][j] == 1:
+                red = (i,j)
+            elif maze[i][j] == 2:
+                blue = (i,j)
+            elif maze[i][j] == 3:
+                red_target = (i,j)
+            elif maze[i][j] == 4:
+                blue_target = (i,j)
 
-    global_minimum = [10**20]  # 리스트로 감싸 global 변수처럼 활용
+    def candidate(cur, visited, target):
+        ret = []
+        if cur == target:
+            return [cur]
+        y,x = cur
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+            if 0<=ny<r and 0<=nx<c and (ny,nx) not in visited and maze[ny][nx]!= 5:
+                ret.append((ny,nx))
+        return ret
 
-    def brute_force(red, blue, turn, red_visited, blue_visited):
-        nonlocal global_minimum
-
-        # global_minimum을 업데이트 하는 조건!!
-        if red == target_red and blue == target_blue:
-            global_minimum[0] = min(global_minimum[0], turn)
+    minimum  = 10 **9
+    def bf(red,blue,turn,red_visited,blue_visited):
+        nonlocal minimum
+        if red == red_target and blue == blue_target:
+            minimum = min(turn,minimum)
             return
+        red_can = candidate(red,red_visited,red_target)
+        blue_can = candidate(blue,blue_visited,blue_target)
+        for new_red in red_can:
+            for new_blue in blue_can:
+                if new_blue== new_red or (new_red==blue and new_blue == red):
+                    continue
+                else:
+                    new_red_visited = red_visited | {new_red}
+                    new_blue_visited = blue_visited | {new_blue}
+                    bf(new_red,new_blue,turn+1,new_red_visited,new_blue_visited)
+    red_visited = {red}
+    blue_visited = {blue}
+    bf(red,blue,0,red_visited,blue_visited)
+    print(minimum)
+    if minimum == 10**9:
+        return 0
+    else:
+        return minimum
 
-        red_candidates = cal_cand(red, maze, red_visited, target_red)
-        blue_candidates = cal_cand(blue, maze, blue_visited, target_blue)
-
-        for rc in red_candidates:
-            for bc in blue_candidates:
-                # 도달한 곳이 같으면 안된다, 자리 바꿔치기를 하면 안된다
-                if rc != bc and (rc, bc) != (blue, red):
-                    new_red_visited = red_visited | {rc}  # 집합 연산을 이용하여 visited 관리
-                    new_blue_visited = blue_visited | {bc}
-
-                    brute_force(rc, bc, turn + 1, new_red_visited, new_blue_visited)
-
-    red_visited, blue_visited = {red}, {blue}
-
-    brute_force(red, blue, 0, red_visited, blue_visited)
-
-    return global_minimum[0] if global_minimum[0] != 10**20 else 0
-
-def find_positions(maze):
-    n, m = len(maze), len(maze[0])
-    for row in range(n):
-        for col in range(m):
-            if maze[row][col] == 1:
-                red = (row, col)
-            elif maze[row][col] == 2:
-                blue = (row, col)
-            elif maze[row][col] == 3:
-                target_red = (row, col)
-            elif maze[row][col] == 4:
-                target_blue = (row, col)
-
-    return red, blue, target_red, target_blue
-
-# cur:본인위치, maze:미로, visited: 본인 visited, target:최종 목적지
-def cal_cand(cur, maze, visited, target):
-    x, y = cur
-    answer = []
-
-    # 이미 도착했으면 도착한 곳을 리턴
-    if cur == target:
-        return [target]
-
-    # 조건 1. 칸을 벗어나지 않는다
-    # 조건 2. visited 딕셔너리에 가려는 칸이 없다
-    # 조건 3. 벽이 아니다
-    for dx, dy in DIR:
-        if 0 <= x + dx < len(maze) and 0 <= y + dy < len(maze[0]) and (x + dx, y + dy) not in visited and \
-                maze[x + dx][y + dy] != 5:
-            answer.append((x + dx, y + dy))
-
-    return answer
+solution([[1, 4], [0, 0], [2, 3]])
