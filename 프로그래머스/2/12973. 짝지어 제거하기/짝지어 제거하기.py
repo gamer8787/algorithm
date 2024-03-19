eaa154869ae5c3cabc7a1c4025a82574f240cc30
gamer8787 <62703390+gamer8787@users.noreply.github.com>
2024-03-20 01:47:54 +0900

    [level 2] Title: 짝지어 제거하기, Time: 85.63 ms, Memory: 14.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,11 @@
+def solution(s):
+    q = []
+    for c in s:
+        if q and q[-1] == c:
+            q.pop()
+        else:
+            q.append(c)
+    if q:
+        return 0
+    else:
+        return 1
