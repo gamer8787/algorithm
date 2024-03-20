91661f4799b3f602447e4a7f8df4c88111aa12b5
gamer8787 <62703390+gamer8787@users.noreply.github.com>
2024-03-20 19:04:10 +0900

    [level 3] Title: 퍼즐 조각 채우기, Time: 0.08 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.py
+++ 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.py
@@ -2,84 +2,96 @@ dy = [-1,0,1,0]
 dx = [0,1,0,-1]
 from collections import deque
 def solution(game_board, table):
-    block_list = []
     N = len(game_board)
-    visited = [[0 for _ in range(N)] for _ in range(N)]
-    def bfs_table(y,x,visited):   
-        visited[y][x] = 1
-        block = [(y,x)]
+    visited_board = [[False for _ in range(N)] for _ in range(N)]
+    def bfs_board(i,j):
+        ret = []
+        visited_board[i][j] = True
+        ret.append([i,j])
         q = deque()
-        q.append((y,x))
+        q.append((i,j))
         while q:
-            y, x = q.popleft()
-            for i in range(4):
-                ny = y + dy[i]
-                nx = x + dx[i]
-                if 0<=ny<N and 0<=nx<N and visited[ny][nx] == 0 and table[ny][nx] !=0:
-                    visited[ny][nx] = 1
-                    q.append((ny,nx))
-                    block.append((ny,nx))
-        block.sort()
-        block1 = sorted(move(block))
-        block2 = move(sorted(rotate(block1)))
-        block3 = move(sorted(rotate(block2)))
-        block4 = move(sorted(rotate(block3)))
-        block_list = [block1,block2,block3,block4]
+            i,j = q.popleft()
+            for k in range(4):
+                ni = i + dy[k]
+                nj = j + dx[k]
+                if 0<=ni<N and 0<=nj<N and game_board[ni][nj]==0 and visited_board[ni][nj]==False:
+                    visited_board[ni][nj]=True
+                    ret.append([ni,nj])
+                    q.append((ni,nj))
+        return ret
     
-        return block_list
-    
-    def bfs_game(y,x,visited2):   
-        visited2[y][x] = 1
-        block = [(y,x)]
+    visited_table = [[False for _ in range(N)] for _ in range(N)]
+    def bfs_table(i,j):
+        ret = []
+        visited_table[i][j] = True
+        ret.append([i,j])
         q = deque()
-        q.append((y,x))
+        q.append((i,j))
         while q:
-            y, x = q.popleft()
-            for i in range(4):
-                ny = y + dy[i]
-                nx = x + dx[i]
-                if 0<=ny<N and 0<=nx<N and visited2[ny][nx] == 0 and game_board[ny][nx] ==0:
-                    visited2[ny][nx] = 1
-                    q.append((ny,nx))
-                    block.append((ny,nx))
-        block.sort()
-        block = move(block)
-        return block
+            i,j = q.popleft()
+            for k in range(4):
+                ni = i + dy[k]
+                nj = j + dx[k]
+                if 0<=ni<N and 0<=nj<N and table[ni][nj]==1 and visited_table[ni][nj]==False:
+                    visited_table[ni][nj]=True
+                    ret.append([ni,nj])
+                    q.append((ni,nj))
+        return ret
     
-    def move(block):
-        y1,x1 = block[0]
-        block = [(y-y1,x-x1) for y,x in block]
-        return block
-    def rotate(block):
-        new_block = [(-x,y) for y,x in block]
-        return new_block
+    board_puzzle = []
+    for i in range(N):
+        for j in range(N):
+            if game_board[i][j] == 0 and visited_board[i][j] == False:
+                r = bfs_board(i,j)
+                board_puzzle.append(r)
+    table_puzzle = []
+    for i in range(N):
+        for j in range(N):
+            if table[i][j] == 1 and visited_table[i][j] == False:
+                r = bfs_table(i,j)
+                table_puzzle.append(r)
+    board_puzzle = [move(i) for i in board_puzzle]
     
-    visited2 = [[0 for _ in range(N)] for _ in range(N)] 
+   
+    table_puzzle0 = [move(i) for i in table_puzzle]
+    table_puzzle1 = [move(rotate(i)) for i in table_puzzle0]
+    table_puzzle2 = [move(rotate(i)) for i in table_puzzle1]
+    table_puzzle3 = [move(rotate(i)) for i in table_puzzle2]
     
-    all_block = []
-    for y in range(N):
-        for x in range(N):
-            if table[y][x]==1 and visited[y][x]==0 :
-                all_block.append(bfs_table(y,x,visited))
-        
-    compare_block = []
-    for y in range(N):
-        for x in range(N):
-            if game_board[y][x] == 0 and visited2[y][x] == 0:
-                a = bfs_game(y,x,visited2)
-                compare_block.append(a)
-    # for block_list in all_block:
-    #     print(block_list)  
-    # print()
-    # for block in compare_block:
-    #     print(block)
-        
-    ret = 0
-    for block in compare_block:
-        for block_list in all_block:
-            if block in block_list:
-                all_block.remove(block_list)
-                ret += len(block)
-                # print(block)
+    avail = set()
+    count = 0
+    for puzzle1 in board_puzzle:
+        for i in range(len(table_puzzle)):
+            if i in avail:
+                continue
+            if puzzle1 == table_puzzle0[i] or puzzle1 == table_puzzle1[i] or puzzle1 == table_puzzle2[i] or puzzle1 == table_puzzle3[i]:
+                avail.add(i)
+                count += len(puzzle1)
                 break
-    return ret
+    print(count)
+    print(avail)
+    answer = -1
+    return count
+
+
+def move(puzzle):
+    puzzle.sort()
+    r,c = puzzle[0]
+    new_puzzle = []
+    for a,b in puzzle:
+        nr = a-r
+        nc = b-c
+        new_puzzle.append([nr,nc])
+    return new_puzzle
+
+def rotate(puzzle):
+    new_puzzle = []
+    for a,b in puzzle:
+        nr = b
+        nc = -a
+        new_puzzle.append([nr,nc])
+    return new_puzzle
+    
+    
+    
