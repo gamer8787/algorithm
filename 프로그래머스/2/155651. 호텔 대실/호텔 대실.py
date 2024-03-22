59164eb2e4712fc3dc46b878a32c2b30f20f6c2a
gamer8787 <62703390+gamer8787@users.noreply.github.com>
2024-03-22 12:38:38 +0900

    [level 2] Title: 호텔 대실, Time: 1.64 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -0,0 +1,15 @@
+def solution(book_time):
+    dp = [0 for _ in range(24*60)]
+    for a,b in book_time:
+        a = change(a)
+        b = change(b)
+        dp[max(a-5,0)]+=1
+        dp[min(b+5,24*60-1)]-=1
+    for i in range(1,24*60):
+        dp[i] = dp[i-1] + dp[i]
+
+    return max(dp)
+
+def change(time):
+    h,m = time.split(":")
+    return 60*int(h)+int(m)
